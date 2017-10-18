e5a1ac91dbeed522d6a186f413812b616391af94
MessaoudBahaa <32883992+MessaoudBahaa@users.noreply.github.com>
2017-10-18 10:13:39 +0100

    Merge branch 'Ouss' of https://github.com/MessaoudBahaa/Application2_tpIGL into Ouss

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,12 +6,38 @@ public class Main {
 	// write your code here
     }
 }
+class MinMax{
+    private int max;
+    private int min;
+    MinMax(){
+        max=0;
+        min=0;
+    }
+    public int getMax() {
+        return max;
+    }
 
+    public int getMin() {
+        return min;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+}
 class VectorHelper{
 
 	 public void Multiplier(int[] vect, int N){
 	     for(int i=0;i<vect.length;i++){
 	         vect[i]=vect[i]*N;
          }
+     }
+     public MinMax GetMinMax(int[] vect){
+        MinMax minMax = new MinMax();
+
      }
 }
