0bca17ac36a8a04fab024f181a2a2984cf4c198b
MessaoudBahaa <32883992+MessaoudBahaa@users.noreply.github.com>
2017-10-18 10:25:28 +0100

    get MaxMin of a vector
    retourne le max et le min d'un vecteur simultanement

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,7 +9,13 @@ public class Main {
 class MinMax{
     private int max;
     private int min;
-    MinMax(){
+
+    public MinMax(int max, int min) {
+        this.max = max;
+        this.min = min;
+    }
+
+    MinMax(int m){
         max=0;
         min=0;
     }
@@ -37,7 +43,17 @@ class VectorHelper{
          }
      }
      public MinMax GetMinMax(int[] vect){
-        MinMax minMax = new MinMax();
-
+	     if(vect.length!=0) {
+             MinMax minMax = new MinMax(vect[0],vect[0]);
+             for (int i = 1; i < vect.length; i++) {
+                if(vect[i]<minMax.getMin()){
+                    minMax.setMin(vect[i]);
+                }else if(vect[i]>minMax.getMax()){
+                    minMax.setMax(vect[i]);
+                }
+             }
+             return minMax;
+         }
+         return null;
      }
 }
