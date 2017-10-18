92847ba79a3ddb2acf8c4c9217585d59fb2f9364
Ouss BOUZ <31183476+ouss23@users.noreply.github.com>
2017-10-18 10:05:37 +0100

    func trier

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,26 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	int[] v = {5,16,54,12,15,2,0};
+        VectorHelper.TrierVect(v);
+        for(int i = 0;i<v.length;i++){
+            System.out.print(v[i]+",");
+        }
     }
 }
 
 class VectorHelper{
-	
+    
+    public static void TrierVect(int[] vect){
+        for(int i = 1;i<vect.length;i++){
+            for(int j = 0;j<i;j++){
+                if(vect[i] < vect[j]){
+                    int a = vect[i];
+                    vect[i] = vect[j];
+                    vect[j] = a;
+                    break;
+                }
+            }
+        }
+    }
 }
