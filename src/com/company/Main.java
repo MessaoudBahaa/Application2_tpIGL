4bc892495203ed2829cd8c09b7c10b37fe314b5f
MessaoudBahaa <32883992+MessaoudBahaa@users.noreply.github.com>
2017-10-24 20:55:22 +0100

    Merge branch 'Messaoud' (Resolution des conflits)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,32 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        //try{
-            //int[] v = VectorHelper.SommerVecteurs(new int[]{5,16,54,12,15,2,0},new int[]{1,5,2});
-            int[] v = {5,16,54,12,15,2,0};
-            System.out.println("Debut :");
-            AffichVect(v);
-            System.out.println("Tri :");
-            VectorHelper.TrierVect(v);
-            AffichVect(v);
-            System.out.println("Inverse :");
-            VectorHelper.InverserVecteur(v);
-            AffichVect(v);
-        /*}
-        catch(DifferentSizeException e){
-            System.err.println("Diff Size Exception !!");
-        }*/
-    }
-    
-    private static void AffichVect(int[] v){
-        for(int i = 0;i<v.length;i++){
-            System.out.print(v[i]+",");
-        }
-        System.out.println("");
     }
 }
 
-class DifferentSizeException extends Exception{};
+class DifferentSizeException extends Exception{}
 
 /**
  * La class de manipulation des vecteurs.
@@ -38,7 +16,7 @@ class VectorHelper{
     /**
      * Trier les elements d'un vecteur en ordre croissant.
      * @param vect 
-     * Le vecteur à trier.
+     * Le vecteur ï¿½ trier.
      */
     public static void TrierVect(int[] vect){
 	boolean en_desordre = true;
@@ -92,4 +70,83 @@ class VectorHelper{
             vect[vect.length - i - 1] = tmp;
         }
     }
+
+    /**
+     * Multiplier un vecteur par un entier
+     * @param vect un vecteur des entiers
+     * @param N un entier
+     * @return le resultat de multiplication de vect par N
+     */
+    public static int[] Multiplier(int[] vect, int N){
+        for(int i=0;i<vect.length;i++){
+            vect[i]=vect[i]*N;
+        }
+        return vect;
+    }
+
+    /**
+     * Retourner le Min et le Max d'un vecteur
+     * @param vect
+     * @return le max et le min du vecteur vect
+     */
+    public static MinMax GetMinMax(int[] vect){
+        if(vect.length!=0) {
+            MinMax minMax = new MinMax(vect[0],vect[0]);
+            for (int i = 1; i < vect.length; i++) {
+                if(vect[i]<minMax.getMin()){
+                    minMax.setMin(vect[i]);
+                }else if(vect[i]>minMax.getMax()){
+                    minMax.setMax(vect[i]);
+                }
+            }
+            return minMax;
+        }
+        return null;
+    }
+
+	// write your code here
+        
+
+    }
+
+
+/**
+ * Une classe qui respresente 2 valeurs entieres, un Min et un Max
+ */
+class MinMax{
+    private int max;
+    private int min;
+
+    public MinMax(int max, int min) {
+        this.max = max;
+        this.min = min;
+    }
+
+    MinMax(){
+        max=0;
+        min=0;
+    }
+    public int getMax() {
+        return max;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+    public void afficher(){
+        System.out.println("le max est : "+max);
+        System.out.println("le min est : "+min);
+    }
+
+
+
 }
+
